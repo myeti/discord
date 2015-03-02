d917008d3fc462712b1196287280f89ce00bb456
Aymeric Assier <aymeric.assier@infomaniak.ch>
2015-03-02 14:24:51 +0100

    Squashed 'Event/' content from commit d554edb
    git-subtree-dir: Event
    git-subtree-split: d554edb33a203fc3e6f1bf4d4143bcaeff9c0875

---
+++ Hub.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Discord\Event;
+
+abstract class Hub
+{
+
+    /** @var Subject */
+    protected static $subject;
+
+
+    /**
+     * Define value expectation
+     *
+     * @param string $event
+     * @param mixed $expectation
+     *
+     * @param bool $continue
+     *
+     * @return mixed
+     */
+    public static function expect($event, $expectation, $continue = false)
+    {
+        return static::subject()->expect($event, $expectation, $continue);
+    }
+
+
+    /**
+     * Attach callable to a specific event
+     *
+     * @param string $event
+     * @param callable $listener
+     *
+     * @return mixed
+     */
+    public static function on($event, callable $listener)
+    {
+        return static::subject()->on($event, $listener);
+    }
+
+
+    /**
+     * Attach listener to subject
+     *
+     * @param object $listener
+     *
+     * @return mixed
+     */
+    public static function attach($listener)
+    {
+        return static::subject()->attach($listener);
+    }
+
+
+    /**
+     * Trigger event
+     *
+     * @param string $event
+     * @param $params
+     *
+     * @return mixed
+     */
+    public static function fire($event, &...$params)
+    {
+        return static::subject()->fire($event, ...$params);
+    }
+
+
+    /**
+     * Get subject instance
+     *
+     * @return Subject
+     */
+    protected static function subject()
+    {
+        if(!static::$subject) {
+            static::$subject = new Channel;
+        }
+
+        return static::$subject;
+    }
+
+}
